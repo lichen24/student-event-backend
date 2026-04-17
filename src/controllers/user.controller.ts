3e3a1bfb9e2f67c6e1eca6af168ad488e5d7a843
lili chen <lili.chen@edu.turkuamk.fi>
2026-04-17 21:08:45 +0300

    create user.controller.ts file and add codes

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,33 @@
+import type { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        role: "USER",
+      },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
