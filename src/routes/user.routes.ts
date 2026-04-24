40a6a4a93f2c836f8af221418afda7f27bcfbf20
lili chen <lili.chen@edu.turkuamk.fi>
2026-04-25 00:37:38 +0300

    update codes in user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { register, login } from "../controllers/user.controller.js";
+import {
+  register,
+  login,
+  getMe,
+} from "../controllers/user.controller.js";
+
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me", authMiddleware, getMe);
 
 export default router;
